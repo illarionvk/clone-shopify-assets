c544872df81c2e8988741e48abebf27b7bd88787
Illarionvk <illarion@outlook.com>
2014-04-04 15:38:54 +0200

    Add function to copy articles to corresponding blog

--- blogs.js
+++ blogs.js
@@ -40,17 +40,12 @@ function getBlogs() {
       uri: baseURL+path },
     function(error, response, body) {
       var blogs = [];
-      var blog = [];
-      var i = 0;
 
       if (!error && response.statusCode === 200) {
         blogs = JSON.parse(body).blogs;
         console.log('Blogs are fetched');
 
-        for (i = 0; i < blogs.length; i += 1) {
-          blog = blogs[i];
-          copyBlog(blog);
-        }
+        copyBlogs(blogs);
 
       } else {
         console.log('Error '+response.statusCode);
@@ -58,26 +53,90 @@ function getBlogs() {
     });
 }
 
-function copyBlog(blog) {
+function copyBlogs(sourceBlogs) {
+  var stdin = process.stdin, stdout = process.stdout;
+  var blog;
   var objectArray = [];
+  var i = 0;
 
-  objectArray.push(blog);
-  console.log(objectArray);
+  for (i = 0; i < sourceBlogs.length; i += 1) {
+    blog = sourceBlogs[i];
+    objectArray.push(blog);
+  }
 
-  //sendItems(objectArray, 'blog', copyArticles, blog);
-  copyArticles(blog);
-}
+  stdin.resume();
+  stdout.write("Do destination blogs exist? (y/n): ");
 
-function copyArticles(sourceBlog) {
-  fp('source', 'blogs/'+sourceBlog.id+'/articles', function() {
-    //fp('destination', 'blogs', function() {
-      //console.log(sourceBlog.id);
-    //}, sourceBlog);
+  stdin.once('data', function(answer) {
+    answer = answer.toString().toLowerCase().trim();
+    if ( answer === 'y' || answer === 'yes' ) {
+      fp('destination', 'blogs', function() {
+        copyArticles(sourceBlogs);
+      });
+    } else if ( answer === 'n' || answer === 'no' ) {
+      sendItems(objectArray, 'blog', function() {
+        fp('destination', 'blogs', copyArticles, sourceBlogs);
+      });
+    } else {
+      console.log('Unknown answer, start the program again');
+    }
   });
 }
 
-getBlogCount();
+function copyArticles(sourceBlogsArray) {
+  var sourceBlog;
+  var i = 0;
 
-//fp('source', 'pages', copyPages);
+  for (i = 0; i < sourceBlogsArray.length; i += 1) {
+    sourceBlog = sourceBlogsArray[i];
+    fp('source', 'blogs/'+sourceBlog.id+'/articles', resolveBlogIDs, sourceBlog);
+  }
+}
+
+function resolveBlogIDs(sourceBlog) {
+  var destination = JSON.parse( fs.readFileSync('./data_destination_blogs.json', { 'encoding': 'utf8' }) );
+  var destBlog;
+  var destBlogID;
+  var i = 0;
+
+  for (i = 0; i < destination.blogs.length; i += 1) {
+    destBlog = destination.blogs[i];
+    if ( destBlog.handle === sourceBlog.handle ) {
+      destBlogID = destBlog.id;
+      console.log('\nBlog handle: '+destBlog.handle);
+      console.log('Source blog ID: '+sourceBlog.id);
+      console.log('Destination blog ID: '+destBlogID);
+    }
+  }
+
+  sendArticles(sourceBlog, destBlogID);
+}
+
+function sendArticles(sourceBlog, destBlogID) {
+  var source = {};
+  var article;
+  var i = 0;
+
+  source.articles = JSON.parse(
+    fs.readFileSync(
+      './data_source_blogs_'+sourceBlog.id+'_articles.json',
+      { encoding: 'utf8' }) ).articles;
+
+  console.log('Total source articles: '+source.articles.length);
+
+  for (i = 0; i < source.articles.length; i += 1) {
+    article = source.articles[i];
+
+    delete article.id;
+    delete article.user_id;
+    delete article.blog_id;
+
+  }
+
+  sendItems(source.articles, 'blogs/'+destBlogID+'/article');
+
+}
+
+getBlogCount();
 
 })();
